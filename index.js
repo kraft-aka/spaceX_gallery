2a0a636a644682468a5e7dc38f028dfe6ce04b4f
kubat <kubat.aiupov@gmail.com>
2025-01-21 21:12:19 +0100

    fetch data

--- index.js
+++ index.js
@@ -3,27 +3,30 @@
 const endpoint = 'https://api.spacexdata.com/v4/rockets'
 
 const spaceData = []
-const list = document.querySelector('.data')
-fetch(endpoint).then(blob => blob.json()).then(data => spaceData.push(data))
+const list = document.querySelector('#data-container')
+fetch(endpoint)
+.then(blob => blob.json())
+.then(data => {
+  spaceData.push(...data);
+  displayData(data)
+})
 
-console.log(spaceData, list)
+console.log(spaceData)
 
-function displayData() {
-  const html = spaceData.map(data => {
+function displayData(spaceData) {
+  const html = spaceData?.map(data => {
+    const imgUrl = data.flickr_images && data.flickr_images.length > 0 ? data.flickr_images[0] : ''
     return `
       <li class='card'>
-        <img src=${data.flickr_images} width='500' height='auto' alt='rocket'/>
+        <img src=${imgUrl} alt='rocket'/>
         <h4>${data.company}</h4>
+        <p>${data.description}</p>
+        <p>${data.country}</p>
+        <p>${data.name}</p>
         </li>
     `
   }).join('')
   list.innerHTML = html
-  console.log(html)
 }
 
-//displayData(); 
-
-
-
-
-
+//window.addEventListener('load',()=>displayData(spaceData))
